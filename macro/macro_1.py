73f901b544d91f88e01802e4f6be09e20bf082ac
Yeonjun-git <lyj09029@gmail.com>
2023-05-11 14:44:46 +0900

    test 1

---
+++ macro/macro_1.py
@@ -0,0 +1,32 @@
+import time 
+import pyautogui 
+
+
+
+cnt = 0
+
+while True:
+    
+    pyautogui.moveTo(100,100)
+    pyautogui.click()
+    cnt+=1
+    print("move(100,100)", cnt)    
+    time.sleep(5)
+
+    pyautogui.moveTo(500,500)
+    pyautogui.click()
+    cnt+=1
+    print("move(600,600)", cnt)
+    time.sleep(5)
+
+    pyautogui.moveTo(1200,1200)
+    pyautogui.click()
+    cnt+=1
+    print("move(1200,1200)", cnt)    
+    time.sleep(5)
+
+    pyautogui.moveTo(1800,1800)
+    pyautogui.click()
+    cnt+=1
+    print("move(1800,1800)", cnt)    
+    time.sleep(5)
